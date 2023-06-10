92a4016c6c19fd0d62a99f5a611f8c85c5a6508f
Michael <kilcorse.michael92@gmail.com>
2023-06-10 10:11:44 -0400

    Change text

--- src/components/AboutSlide.tsx
+++ src/components/AboutSlide.tsx
@@ -7,16 +7,22 @@ export default function AboutSlide() {
     <div className="flex grow flex-col md:flex-row items-center justify-center h-screen">
       <div className="grow w-full h-full bg-white text-center">
         <Parallax opacity={[0, 1]}>
-          <h1 className="animate-fade-in text-5xl text-cyan-600">ABOUT</h1>
+          <h1 className="animate-fade-in text-5xl lg:text-7xl text-cyan-600">
+            <span className="text-8xl lg:text-9xl content-start">A</span>
+            <span className=" mb-80">BOUT</span>
+          </h1>
           <p className="m-10 text-xl lg:text-6xl text-violet-900">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil odit
-            iusto facilis officiis asperiores non quia labore corporis, nobis
-            dolorem consequuntur rerum autem fuga ipsa deserunt ipsum, sit
-            commodi tempore?
+            <span className="text-3xl lg:text-8xl">L</span>orem ipsum dolor, sit
+            amet consectetur adipisicing elit. Nihil odit iusto facilis officiis
+            asperiores non quia labore corporis, nobis dolorem consequuntur
+            rerum autem fuga ipsa deserunt ipsum, sit commodi tempore?
           </p>
           <div className="">
             <Link to="/about">
-              <button type="button" className="bg-teal-400 border-black border-b-2 p-2 font-normal rounded-md text-black">
+              <button
+                type="button"
+                className="bg-teal-400 border-black border-b-2 p-2 font-normal rounded-md text-black"
+              >
                 READ MORE
               </button>
             </Link>
@@ -24,7 +30,7 @@ export default function AboutSlide() {
         </Parallax>
       </div>
       <div className=" flex w-full h-full bg-black">
-        <img src={profilePic} />
+        <img alt="Michael Kilcorse" src={profilePic} />
       </div>
     </div>
   );
--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -1,30 +1,40 @@
-import { ParallaxBanner} from 'react-scroll-parallax'
-import frontImg from '../assets/2.png'
-import backImg from '../assets/1.png'
+import { ParallaxBanner } from "react-scroll-parallax";
+import frontImg from "../assets/2.png";
+import backImg from "../assets/1.png";
 
-export default function HeroIntro(){
-
-    return(
-        <>
-        <ParallaxBanner
+export default function HeroIntro() {
+  return (
+    <>
+      <ParallaxBanner
         layers={[
-            { image: backImg, speed: -50 },
-            {
+          { image: backImg, speed: -50 },
+          {
             speed: -25,
             children: (
-                <div className="absolute m-2 inset-0 flex flex-col text-center items-center justify-center">
-                <h1 className="animate-fade-in text-6xl md:text-8xl text-white font-thin">MICHAEL KILCORSE</h1>
-                <h1 className='animate-fade-in opacity-60 text-2xl md:text-4xl text-white font-thin'>Designer Developer Innovator</h1>
-                </div>
+              <div className="absolute animate-fade-in m-2 inset-0 flex flex-col text-center items-center justify-center">
+                <p className="text-slate-400 mb-3 tracking-wide">
+                  Hello! My name is
+                </p>
+                <h1 className=" text-6xl md:text-8xl text-white tracking-wider font-thin">
+                  MICHAEL KILCORSE
+                </h1>
+                <p className="text-slate-400 mb-3 tracking-wide -mb-1">
+                  I am a{" "}
+                </p>
+                <h1 className="text-2xl md:text-4xl text-white font-thin">
+                  Designer & Developer
+                </h1>
+                <p className="text-slate-400 text-sm mb-3 tracking-wider">
+                  (and I really like space)
+                </p>
+              </div>
             ),
-            },
-            { image: frontImg, speed: 5 },
+          },
+          { image: frontImg, speed: 5 },
         ]}
         className="h-screen w-screen"
-        />
-        <div className='absolute flex text-center inset-0'>
-            
-        </div>
+      />
+      <div className="absolute flex text-center inset-0"></div>
     </>
-    )
+  );
 }
